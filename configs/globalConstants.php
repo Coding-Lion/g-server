727143619a13119961c87d179e7045e1df4ad7a3
Saito88 <marcel.meier@check24.de>
2017-09-02 21:10:48 +0200

    [VERSPHP-003] Add global constants and db params

---
+++ configs/globalConstants.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Saito88
+ * Date: 29.08.2017
+ * Time: 09:38
+ */
+
+const DB_FRONTEND_CONFIG = [
+    'host'      => 'localhost',
+    'user'      => 'root',
+    'password'  => '',
+    'dbname'    => 'g-server',
+    'prefix'    => 'web_',
+    'port'      => 3306,
+    'socket'    => 'false',
+];
+
+const DB_BACKEND_CONFIG = [
+    'host'      => 'localhost',
+    'user'      => 'root',
+    'password'  => '',
+    'dbname'    => 'g-server',
+    'prefix'    => '',
+    'port'      => 3306,
+    'socket'    => 'false',
+];
