8202c9b9ec55c11b26dfa68c15550fb930c66cd7
Saito88 <marcel.meier@check24.de>
2017-09-03 20:27:25 +0200

    [VERSPHP-008] Add final process for all requests

--- g-server.php
+++ g-server.php
@@ -6,4 +6,33 @@
  * Time: 23:22
  */
 
+/**
+ * Load all necessary files
+ */
 require_once('autoload.php');
+
+/**
+ * @var gserver\core\Request
+ */
+$Request = Gserver()->Request();
+
+/**
+ * @var gserver\controllers\
+ */
+$Controller = $Request->getController();
+
+/**
+ * @var array
+ */
+$params = $Request->getParams();
+
+/**
+ * @var gserver\core\TextModule
+ */
+$TextModule = $Controller->getTextModule();
+
+/**
+ * Includes the requested source code
+ */
+$Controller->loadTemplate();
+
